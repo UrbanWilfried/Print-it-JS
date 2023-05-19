bd6cf96c3f7387caa0a5101f04cf56bf1bccfafe
Wilfried Urban <wilfriedu@gmail.com>
2023-05-19 14:25:28 +0200

    Création du bullet points pour le carrousel

--- assets/script.js
+++ assets/script.js
@@ -19,6 +19,7 @@
 */
 
 const items = document.querySelectorAll('.carousel')
+const items2 = document.querySelectorAll('.dot')
 const nbSlide = items.length
 const next = document.querySelector('.arrow_right')
 const previous = document.querySelector('.arrow_left')
@@ -26,24 +27,28 @@ let count = 0
 
 function slideNext(){
 	items[count].classList.remove('active')
+	items2[count].classList.remove('dot_selected')
 	if(count < nbSlide -1){
 		count++
 	} else {
 		count = 0
 	}
 	items[count].classList.add('active')
+	items2[count].classList.add('dot_selected')
 	console.log(count)
 }
 next.addEventListener('click', slideNext)
 
 function slidePrevious(){
 	items[count].classList.remove('active')
+	items2[count].classList.remove('dot_selected')
 	if(count > 0){
 		count--
 	} else {
 		count = nbSlide - 1
 	}
 	items[count].classList.add('active')
+	items2[count].classList.add('dot_selected')
 	console.log(count)
 }
 previous.addEventListener('click', slidePrevious)
